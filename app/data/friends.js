cbef1b025e5864439e3c21f69c0e2599f6b80554
JeromeHD <jaymagolan@gmail.com>
2019-04-02 20:03:29 -0400

    friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,27 +2,27 @@
 var friends = [
   {
     name: "Mr T.",
-    photo: "",
+    photo: "https://pbs.twimg.com/media/DiT-EjmUYAMLkCS.jpg",
     scores: ["5", "3", "5", "5", "2", "5", "4", "2", "1", "5"]
   },
   {
     name: "Rocky Balboa",
-    photo: "",
+    photo: "https://upload.wikimedia.org/wikipedia/en/thumb/5/53/Rocky_balboa.jpeg/200px-Rocky_balboa.jpeg",
     scores: ["3", "5", "1", "3", "1", "5", "1", "3", "2", "4"]
   },
   {
     name: "Predator",
-    photo: "",
+    photo: "https://i.ytimg.com/vi/Y1txEAywdiw/hqdefault.jpg",
     scores: ["1", "2", "4", "1", "5", "2", "3", "5", "4", "2"]
   },
   {
-    name: "Great Ape",
-    photo: "",
+    name: "Grape Ape",
+    photo: "https://static.tvtropes.org/pmwiki/pub/images/great_grape_ape_7584.jpeg",
     scores: ["5", "2", "5", "2", "4", "1", "4", "4", "5", "3"]
   },
   {
     name: "DeadPool",
-    photo: "",
+    photo: "https://cdn-images-1.medium.com/max/1920/1*0ubYRV_WNR9iYrzUAVINHw.jpeg",
     scores: ["3", "4", "4", "1", "5", "4", "5", "5", "5", "5"]
   }
 ];
